f3763d1744a9e745ac9226174ce3daa0f222f95d
showianchensc <showianchen.sc@gmail.com>
2026-05-10 01:27:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/webhook", line.middleware(config), async (req, res) => {
       const userMessage = event.message.text;
 
       const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.5-flash",
       });
 
       const result = await model.generateContent(`

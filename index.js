95e797ff84b2d4f2fc27839ec9982ddec778de8c
showianchensc <showianchen.sc@gmail.com>
2026-05-10 19:40:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const auth = new google.auth.JWT(
   process.env.GOOGLE_CLIENT_EMAIL,
   null,
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  (process.env.GOOGLE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
   ["https://www.googleapis.com/auth/spreadsheets"]
 );
 
